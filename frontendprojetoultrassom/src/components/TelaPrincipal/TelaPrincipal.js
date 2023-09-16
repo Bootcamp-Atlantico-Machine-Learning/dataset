b057401816a0223f3332ef330a1b06f66f3bc598
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2023-09-16 13:24:31 -0300

    frontend

--- frontendprojetoultrassom/src/components/TelaPrincipal/TelaPrincipal.js
+++ frontendprojetoultrassom/src/components/TelaPrincipal/TelaPrincipal.js
@@ -1,8 +1,38 @@
+import react ,{ useState } from 'react'
 
-export default function TelaPrincipal(){
+export default function TelaPrincipal()
+{
+    const [TextError , SetTextoError] = useState('');
+    const [File      , SetFile      ] = useState();
+
+    function PostImageTurmo( image ){
+        alert('imagem em upload') 
+        try{
+            SetTextoError('');
+            SetFile(
+                URL.createObjectURL( 
+                    image.target.files[0]
+                    )
+
+            );
+
+        }catch(error){
+            SetTextoError('Invalido tente novamente');
+        }
+    }
     return(
         <div>
-            <h1> hello world</h1>
+            <h1>Projeto Breast Ultrasound</h1>
+
+            <div>
+                <p>{TextError}</p>
+                <img  src={File}/>
+            </div>
+            <div>
+                <input type="file" onChange={PostImageTurmo}></input>
+            </div>
+        
+
         </div>
     );
 }
