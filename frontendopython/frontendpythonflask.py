c7f479e49b809ce5a25b74911218fe7a7890cdfb
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2023-09-22 21:43:39 -0300

    dataset

--- frontendopython/frontendpythonflask.py
+++ frontendopython/frontendpythonflask.py
@@ -3,22 +3,32 @@ import os
 
 app = Flask(__name__)
 
-
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'backend/imagens'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return "Nenhum arquivo enviado"
+            return "Escolha uma imagem raio x"
 
         file = request.files['file']
 
         if file.filename == '':
             return "Nome do arquivo vazio"
 
+        allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+        if '.' not in file.filename or file.filename.split('.')[-1].lower() not in allowed_extensions:
+            return "Arquivo não é existe ou não foi baixado"
+
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+
+        return render_template('upload.html', uploaded_image=file_path)
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
